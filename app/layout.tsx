839823fe8d5f402dae89ef38f268aa670fbd1bc3
thedemonsid <siddheshshrirame@gmail.com>
2025-02-18 01:04:44 +0530

    feat : added the floating navbar and some minor changes about theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import FloatingNavbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`font-sans antialiased`}
-      >
+      <body className={`font-sans antialiased`}>
         {children}
+        <FloatingNavbar></FloatingNavbar>
       </body>
     </html>
   );
